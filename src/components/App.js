b0c0601e4ee3e454bd43036a4c8722752aa572d4
sandipjha762 <sandipjha762@gmail.com>
2021-05-22 16:06:32 +0530

    a

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,21 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <form>
+        <input data-testid = 'name' type="text" placeholder="Name"/><br></br>
+        <input data-testid = 'email' type="email" placeholder="Email ID"/><br></br>
+        <label for="gender">Gender</label>
+          <select name="gender" id="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select><br></br>
+        <input data-testid = 'phoneNumber' type="number" placeholder="Phone Number"/><br></br>
+        <input data-testid = 'password' type="password" placeholder="Password"/><br></br>
+        <button data-testid = 'submit' type="button">Submit</button>
+      </form>
+      
+    </div>
   )
 }
 
