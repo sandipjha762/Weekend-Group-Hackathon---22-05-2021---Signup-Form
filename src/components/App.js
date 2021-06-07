14361b4b5638768275aa91952e958bb564d786b4
sandipjha762 <52753398+sandipjha762@users.noreply.github.com>
2021-06-07 16:57:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,88 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState,useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const [name, setname] = useState('');
+  const [email, setemail] = useState('');
+  const [gender, setgender] = useState('male');
+  const [phone, setphone] = useState('');
+  const [password, setpassword] = useState('');
+  const [success, setsuccess] = useState(false);
+  const [message, setmessage] = useState('');
+  const [error, seterror] = useState(null)
+  useEffect(() => {
+    setemail('');
+    setgender('');
+    setphone('');
+    setpassword('');
+    setname('');
+  }, [success])
+  const submitForm = (e) => {
+    e.preventDefault();
+    if (name === '' && email === '' && gender === '' && phone === '' && password === '') {
+      alert("All fields are mandatory");
+    }
+    else {
+      if (name === '') { seterror("Name Error") }
+      else if (email === '') { seterror("Email Error") }
+      else if (phone === '') { seterror("Phone Number Error") }
+      else if (password === '') { seterror("Password Error") }
+      else {
+        let alphaExp = /^([a-z]+[\s]*[0-9]+[\s]*)+$/i;
+        if (!alphaExp.test(name)) {
+          seterror("Name is not alphanumeric")
+        }
+        else if (!email.includes('@')) {
+          seterror("Email must contain @")
+        }
+        else if (gender === "") {
+          seterror("Please identify as male, female or others")
+        }
+        else if (isNaN(phone)) {
+          seterror("Phone Number must contain only numbers")
+        }
+        else if (password.length < 6) {
+          seterror("Password must contain atleast 6 letters")
+        } else {
+          setsuccess(true);
+          seterror(null);
+          let username = email.split("@");
+          setmessage(`Hello ${username[0]}`);
+        }
+      }
+    }
+  }
   return (
     <div id="main">
-      <form>
-        <input data-testid = 'name' type="text" placeholder="Name"/><br></br>
-        <input data-testid = 'email' type="email" placeholder="Email ID"/><br></br>
-        <label for="gender">Gender</label>
-          <select name="gender" id="gender">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select><br></br>
-        <input data-testid = 'phoneNumber' type="number" placeholder="Phone Number"/><br></br>
-        <input data-testid = 'password' type="password" placeholder="Password"/><br></br>
-        <button data-testid = 'submit' type="button">Submit</button>
-      </form>
-      
+        <form>
+      {!success && error}
+      {success && message}
+        <div className="mb-3">
+        <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" data-testid="name" onChange={(e)=>setname(e.target.value)}/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email">Email address</label>
+          <input type="email" className="form-control" data-testid="email" onChange={(e)=>setemail(e.target.value)}/>
+        </div>
+        <div className="mb-3">
+        <label htmlFor="email">Gender</label>
+          <select className="form-select" data-testid = 'gender' onChange={(e)=>setgender(e.target.value)}>
+            <option value="male">male</option>
+            <option value="female" >female</option>
+            <option value="other">other</option>
+         </select>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="phone">Phone Number</label>
+          <input type="number" className="form-control" data-testid = 'phoneNumber' onChange={(e)=>setphone(e.target.value)}/>
+        </div>
+        <div className="mb-3">
+        <label htmlFor="phone">Password</label>
+          <input type="password" className="form-control" data-testid = 'password' onChange={(e)=>setpassword(e.target.value)}/>
+        </div>
+        <button type="submit" className="btn btn-primary" data-testid = 'submit' onClick={submitForm}>Submit</button>
+    </form>
     </div>
   )
 }
